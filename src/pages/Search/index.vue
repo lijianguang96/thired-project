e46ed8c3afa75e769f161904dd45d5b23eb4cf35
Ylljjgg <2322315384@qq.com>
2021-06-07 11:59:14 +0800

    完成搜索

--- src/pages/Search/index.vue
+++ src/pages/Search/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="search">
-    <!-- <van-icon name="arrow-left" size="20" /> -->
+    <van-icon name="arrow-left" size="20" @click="back" />
     <van-search
       v-model="value"
       shape="round"
-      background="#4fc08d"
+      background="#fff"
       placeholder="请输入搜索关键词"
     />
-    <!-- <span class="searchbtn">搜索</span> -->
+    <span class="searchbtn">搜索</span>
   </div>
 </template>
 
@@ -21,7 +21,11 @@ export default {
   },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    back() {
+      this.$router.back();
+    },
+  },
   created() {},
   mounted() {},
 };
@@ -37,12 +41,19 @@ export default {
 .van-icon {
   padding: 0 5px;
 }
-.searchbtn {
-  width: 10px;
+.van-search {
+  width: 260px;
   height: 30px;
 }
-.van-search {
-  width: 200px;
-  height: 30px;
+.searchbtn {
+  width: 40px;
+  height: 28px;
+  font-size: 14px;
+  color: #fff;
+  background: #e93b3d;
+  border-radius: 5px;
+  text-align: center;
+  line-height: 28px;
+  margin-left: 10px;
 }
 </style>
